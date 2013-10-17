0d27bfccf9ee5ff3aabc7a57b5307697adb29e9f
mbranko@uns.ac.rs <mbranko@uns.ac.rs>
2013-10-17 23:48:58 +0200

    Postavljen treci primer.

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ MBS2
 ====
 
 Primeri sa predavanja iz Mrežno baziranih sistema 2.
+
+
+Potrebne stvari
+---------------
+
+1. Apache Ant 1.9+
+
+1. Apache TomEE+ 1.5+
+
+
+Podešavanje
+-----------
+
+1. Podesiti putanju do Apache TomEE+ instalacije u fajlu
+build.properties.
+
---
+++ src/mbs2/pr03/LDAPClient.java
@@ -0,0 +1,44 @@
+package mbs2.pr03;
+
+import java.util.Properties;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.BasicAttribute;
+import javax.naming.directory.BasicAttributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchResult;
+
+public class LDAPClient {
+  public static void main(String[] args) {
+    try {
+      Properties env = new Properties();
+      // klasa za kreiranje inicijalnog konteksta
+      env.put("java.naming.factory.initial",
+        "com.sun.jndi.ldap.LdapCtxFactory");
+      // adresa LDAP servera i inicijalni kontekst
+      env.put("java.naming.provider.url",
+        "ldap://ldap.ftninformatika.com:389");
+      // kreiramo inicijalni kontekst
+      DirContext ctx = new InitialDirContext(env);
+
+      // Navodimo koje atribute trazimo
+      Attributes match = new BasicAttributes(true); // ignore case
+      match.put(new BasicAttribute("uid", "branko.milosavljevic"));
+
+      // Postavljamo upit u okviru podkonteksta
+      // dc=informatika, dc=com
+      NamingEnumeration<SearchResult> answer = ctx.search(
+          "dc=ftninformatika,dc=com", match);
+
+      // ispisujemo rezultate upita
+      while (answer.hasMore()) {
+        SearchResult sr = answer.next();
+        System.out.println("Objekat: " + sr.getAttributes());
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+}
