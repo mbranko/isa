37b8b5db0a5ddda8141cde25306b88b8fb5c68cc
mbranko@uns.ac.rs <mbranko@uns.ac.rs>
2013-11-09 03:48:27 +0100

    Dodat primer 24.

---
+++ src/mbs2/pr24/queue/PaymentBean.java
@@ -0,0 +1,31 @@
+package mbs2.pr24.queue;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+@MessageDriven(activationConfig={
+    @ActivationConfigProperty(
+        propertyName="destination",
+        propertyValue="PaymentQueue"),
+    @ActivationConfigProperty(
+        propertyName="destinationType",
+        propertyValue="javax.jms.Queue")
+})
+public class PaymentBean implements MessageListener {
+  
+  public void onMessage(Message msg) {
+    if (msg instanceof ObjectMessage) {
+      try {
+        ObjectMessage obj = (ObjectMessage)msg;
+        PaymentInfo info = (PaymentInfo)obj.getObject();
+        System.out.println(info);
+      } catch (JMSException ex) {
+        ex.printStackTrace();
+      }
+    }
+  }
+}
---
+++ src/mbs2/pr24/readme.txt
@@ -0,0 +1,16 @@
+FTN / Katedra za informatiku
+Mrezno bazirani sistemi 2 / 2013.
+=================================
+
+1. Lokacija primera
+-------------------
+mbs2.pr24
+
+2. Opis primera
+---------------
+Primer MDB-a, uz citanje poruka iz JMS queuea i topica.
+
+3. Sadrzaj primera
+------------------
+queue.*   - primer MDB-a (i klijenta) koji cita poruke iz JMS queue-a
+topic.*   - primer MDB-a (i klijenta) koji cita poruke iz JMS topic-a
